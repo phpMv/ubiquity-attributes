3ac0c8d0734c8082b8cb4a5bf205b878c762a7e4
jcheron <myaddressmail@gmail.com>
2020-12-13 23:59:48 +0100

    add getNamespace

--- src/Ubiquity/attributes/items/BaseAttribute.php
+++ src/Ubiquity/attributes/items/BaseAttribute.php
@@ -15,14 +15,24 @@ use Ubiquity\utils\base\UArray;
 abstract class BaseAttribute {
 	use BaseAnnotationTrait;
 
+	protected \ReflectionClass $reflect;
+
 	protected function asAnnotation(): string {
 		$fields = $this->getPropertiesAndValues();
 		return UArray::asPhpAttribute($fields);
 	}
 
+	protected function getReflectionClass(): \ReflectionClass {
+		return $this->reflect ??= new \ReflectionClass($this);
+	}
+
+	public function getNamespace(): string {
+		return $this->getReflectionClass()->getNamespaceName();
+	}
+
 	public function __toString(): string {
 		$extsStr = $this->asAnnotation();
-		$className = (new \ReflectionClass($this))->getShortName();
+		$className = $this->getReflectionClass()->getShortName();
 		return '#[' . $className . $extsStr . ']';
 	}
 }
