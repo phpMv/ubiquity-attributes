71cc5b124cfdb6050146cfdb57b6a858ae61b2a5
jcheron <myaddressmail@gmail.com>
2020-12-14 19:02:12 +0100

    Update AttributesEngine.php

--- src/Ubiquity/attributes/AttributesEngine.php
+++ src/Ubiquity/attributes/AttributesEngine.php
@@ -20,19 +20,22 @@ class AttributesEngine implements AnnotationsEngineInterface {
 		return $result;
 	}
 
-	public function getAnnotsOfClass(string $class, string $annotationType = null): array {
+	public function getAnnotsOfClass(string $class, ?string $annotationType = null): array {
 		$reflect = new \ReflectionClass($class);
-		return $this->attributesNewInstances($reflect->getAttributes($annotationType));
+		$annotClass = $this->getAnnotationByKey($annotationType);
+		return $this->attributesNewInstances($reflect->getAttributes($annotClass));
 	}
 
-	public function getAnnotsOfMethod(string $class, string $method, string $annotationType = null): array {
+	public function getAnnotsOfMethod(string $class, string $method, ?string $annotationType = null): array {
 		$reflect = new \ReflectionMethod($class, $method);
-		return $this->attributesNewInstances($reflect->getAttributes($annotationType));
+		$annotClass = $this->getAnnotationByKey($annotationType);
+		return $this->attributesNewInstances($reflect->getAttributes($annotClass));
 	}
 
-	public function getAnnotsOfProperty(string $class, string $property, string $annotationType = null): array {
+	public function getAnnotsOfProperty(string $class, string $property, ?string $annotationType = null): array {
 		$reflect = new \ReflectionProperty($class, $property);
-		return $this->attributesNewInstances($reflect->getAttributes($annotationType));
+		$annotClass = $this->getAnnotationByKey($annotationType);
+		return $this->attributesNewInstances($reflect->getAttributes($annotClass));
 	}
 
 	public function start(string $cacheDirectory): void {
@@ -48,11 +51,12 @@ class AttributesEngine implements AnnotationsEngineInterface {
 		];
 	}
 
-	public function getAnnotationByKey(string $key): ?string {
+	public function getAnnotationByKey(?string $key = null): ?string {
 		return $this->register[$key] ?? null;
 	}
 
 	public function registerAnnotations(array $nameClasses): void {
+		\array_merge($this->register, $nameClasses);
 	}
 }
 
