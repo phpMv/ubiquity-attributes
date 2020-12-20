26bb66592ab799cf5f1c2b6a3ae26a4ea970f22a
jcheron <myaddressmail@gmail.com>
2020-12-20 19:27:46 +0100

    Update JoinColumn.php

--- src/Ubiquity/attributes/items/JoinColumn.php
+++ src/Ubiquity/attributes/items/JoinColumn.php
@@ -16,9 +16,9 @@ use Attribute;
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class JoinColumn extends Column {
 	public string $className;
-	public string $referencedColumnName;
+	public ?string $referencedColumnName;
 
-	public function __construct(string $name, string $className, string $referencedColumnName, bool $nullable = false, string $dbType = '') {
+	public function __construct(string $name, string $className, ?string $referencedColumnName = null, ?bool $nullable = false, ?string $dbType = '') {
 		parent::__construct($name, $nullable, $dbType);
 		$this->className = $className;
 		$this->referencedColumnName = $referencedColumnName;
