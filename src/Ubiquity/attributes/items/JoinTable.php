a7f0c6d3899eb6715a879c2c9a28b67d89158869
jcheron <myaddressmail@gmail.com>
2021-03-07 02:35:41 +0100

    Fix joinTable pb

--- src/Ubiquity/attributes/items/JoinTable.php
+++ src/Ubiquity/attributes/items/JoinTable.php
@@ -9,21 +9,21 @@ use Attribute;
  * Annotation JoinTable.
  * usages :
  * - #[JoinTable(name: "tableName")]
- * - #[JoinTable(name: "tableName",joinColumns: "fieldname")]
- * - #[JoinTable(name: "tableName",joinColumns: "fieldname",inverseJoinColumns: "fieldname")]
+ * - #[JoinTable(name: "tableName",joinColumns: ["fieldname"=>"value"])]
+ * - #[JoinTable(name: "tableName",joinColumns: ["fieldname"=>"value"],inverseJoinColumns: ["fieldname"=>"value"])]
  *
  * @author jc
- * @version 1.0.0
+ * @version 1.0.1
  */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class JoinTable extends BaseAttribute {
 	use BaseAnnotationTrait;
 
 	public string $name;
-	public ?string $joinColumns;
-	public ?string $inverseJoinColumns;
+	public ?array $joinColumns;
+	public ?array $inverseJoinColumns;
 
-	public function __construct(string $name, ?string $joinColumns = null, ?string $inverseJoinColumns = null) {
+	public function __construct(string $name, ?array $joinColumns = null, ?array $inverseJoinColumns = null) {
 		$this->name = $name;
 		$this->joinColumns = $joinColumns;
 		$this->inverseJoinColumns = $inverseJoinColumns;
