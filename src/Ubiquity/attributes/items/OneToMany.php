169d1a3c42658d32c4d00f21290767f8d7d66303
jcheron <myaddressmail@gmail.com>
2021-06-19 01:49:37 +0200

    Add cascade to OneToMany

--- src/Ubiquity/attributes/items/OneToMany.php
+++ src/Ubiquity/attributes/items/OneToMany.php
@@ -9,19 +9,21 @@ use Attribute;
  * Annotation OneToMany.
  * usage :
  * - #[OneToMany(mappedBy:"memberName",className:"classname")]
+ * - #[OneToMany(mappedBy:"memberName",className:"classname",cascade:["remove"])]
  *
  * @author jc
- * @version 1.0.0
+ * @version 1.0.1
  */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class OneToMany extends BaseAttribute {
 	use BaseAnnotationTrait;
-	
+
 	public string $mappedBy;
 	public string $className;
-	
-	public function __construct(string $mappedBy,string $className){
-		$this->mappedBy=$mappedBy;
-		$this->className=$className;
+	public ?array $cascade;
+
+	public function __construct(string $mappedBy, string $className) {
+		$this->mappedBy = $mappedBy;
+		$this->className = $className;
 	}
 }
