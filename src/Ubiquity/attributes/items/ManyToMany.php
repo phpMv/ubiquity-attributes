21d2eb27bb2666889bee5070c34818ce2030a2be
jcheron <myaddressmail@gmail.com>
2021-06-19 02:50:35 +0200

    Add cascade to ManyToMany

--- src/Ubiquity/attributes/items/ManyToMany.php
+++ src/Ubiquity/attributes/items/ManyToMany.php
@@ -9,11 +9,12 @@ use Attribute;
  * Annotation ManyToMany.
  * usages :
  * - #[ManyToMany(targetEntity: "classname")]
- * - #[ManyToMany(targetEntity: "classname",inversedBy: "memberName")]
- * - #[ManyToMany(targetEntity: "classname",inversedBy: "memberName",mappedBy: "memberName")]
+ * - #[ManyToMany(targetEntity: "classname", inversedBy: "memberName")]
+ * - #[ManyToMany(targetEntity: "classname", inversedBy: "memberName", mappedBy: "memberName")]
+ * - #[ManyToMany(targetEntity: "classname", inversedBy: "memberName", mappedBy: "memberName", cascade: ["remove"])]
  *
  * @author jc
- * @version 1.0.0
+ * @version 1.0.1
  */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class ManyToMany extends BaseAttribute {
@@ -22,10 +23,12 @@ class ManyToMany extends BaseAttribute {
 	public string $targetEntity;
 	public ?string $inversedBy;
 	public ?string $mappedBy;
+	public ?array $cascade;
 
-	public function __construct(string $targetEntity, ?string $inversedBy = null, ?string $mappedBy = null) {
+	public function __construct(string $targetEntity, ?string $inversedBy = null, ?string $mappedBy = null, ?array $cascade = null) {
 		$this->targetEntity = $targetEntity;
 		$this->inversedBy = $inversedBy;
 		$this->mappedBy = $mappedBy;
+		$this->cascade = $cascade;
 	}
 }
